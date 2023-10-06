0a1da08454b23bee231a806825cb803facad569e
Sim0ndav <si.char.david@gmail.com>
2023-10-06 09:39:38 +0200

    first commit

---
+++ src/main/java/fr/simondav/App.java
@@ -0,0 +1,27 @@
+package fr.simondav;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+
+        int salaireDeBase = 400;
+        int primePotentielle = 250;
+        int quota = 10;
+
+        System.out.println("Veuillez saisir votre nombre de ventes cette semaine :");
+        int nbDeVentes = clavier.nextInt();
+        clavier.close();
+
+        double salaireFinal = salaireDeBase;
+
+        if (nbDeVentes >= quota) {
+            salaireFinal = salaireDeBase + primePotentielle;
+        }
+
+        System.out.println("le salaire est de " + salaireFinal + " euros ");
+    }
+
+}
